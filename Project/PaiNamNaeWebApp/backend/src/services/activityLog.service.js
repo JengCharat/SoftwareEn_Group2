eda766640b5ff8694a1b3dde709d555971640a38
JengCharat <zazajeng401@gmail.com>
2026-02-15 01:25:42 +0000

    can show username

--- Project/PaiNamNaeWebApp/backend/src/services/activityLog.service.js
+++ Project/PaiNamNaeWebApp/backend/src/services/activityLog.service.js
@@ -2,8 +2,15 @@ const prisma = require("../utils/prisma");
 
 const getAllLog = async () => {
   const logs = await prisma.activityLog.findMany({
-    orderBy: {
-      createdAt: "desc",
+    orderBy: { createdAt: "desc" },
+    include: {
+      user: {
+        select: {
+          id: true,
+          email: true,
+          username: true,
+        },
+      },
     },
   });
 
