a290e1a6c850817f196aefdcbc89d6b81845eac2
JengCharat <zazajeng401@gmail.com>
2026-02-16 20:49:08 +0000

    use login function as a observer to active account again

---
+++ Project/PaiNamNaeWebApp/backend/src/services/blacklist.service.js
@@ -0,0 +1,57 @@
+import prisma from "../utils/prisma.js";
+import ApiError from "../utils/ApiError.js";
+
+export const checkUserAccess = async (userId) => {
+  const user = await prisma.user.findUnique({
+    where: { id: userId },
+    select: {
+      id: true,
+      nationalIdNumber: true,
+    },
+  });
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  if (!user.nationalIdNumber) return;
+
+  const blacklist = await prisma.blacklist.findFirst({
+    where: {
+      nationalIdNumber: user.nationalIdNumber,
+    },
+  });
+
+  if (!blacklist) return;
+
+  const now = new Date();
+  console.log("===== BLACKLIST CHECK =====");
+  console.log("now:", now);
+  console.log("now ISO:", now.toISOString());
+
+  console.log("expireAt:", blacklist.expiresAt);
+  console.log(
+    "expireAt ISO:",
+    blacklist.expiresAt ? blacklist.expiresAt.toISOString() : null,
+  );
+
+  console.log(
+    "isExpired:",
+    blacklist.expiresAt ? blacklist.expiresAt < now : "no expire",
+  );
+
+  console.log("===========================");
+
+  if (!blacklist.expiresAt || blacklist.expiresAt > now) {
+    throw new ApiError(403, "Your account has been suspended.");
+  }
+
+  await prisma.user.update({
+    where: { id: user.id },
+    data: { isActive: true },
+  });
+
+  await prisma.blacklist.delete({
+    where: { id: blacklist.id },
+  });
+};
